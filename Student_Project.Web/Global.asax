9eb2851cbd561e25d33b819d311036bcb5d57fd4
Paras Sharma <sharmaparas1117@gmail.com>
2016-11-16 17:27:31 +1100

    MVC project added
    Added a MVC project and deleted the empty one

---
+++ Student_Project.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Student_Project.Web.MvcApplication" Language="C#" %>
---
+++ Student_Project.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Student_Project.Web.Startup))]
+namespace Student_Project.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
