3e703bb6abab478a140dada6c583d323fc5de6c6
Paras Sharma <sharmaparas1117@gmail.com>
2016-11-18 15:19:37 +1100

    updated angular views
    further updated views, to use entityframe work to store data

--- Student_Project.Web/Scripts/custom_angular.js
+++ Student_Project.Web/Scripts/custom_angular.js
@@ -2,9 +2,21 @@
     var app = angular.module("studentdata", []);
     var MainController = function ($scope) {
         $scope.imageData = [
-                            { src: '../Content/image/student_add_image.jpg', description: 'Add a student' , id:0},
-                            { src: '../Content/image/data_view.jpg', description: 'Retrieve student data' , id:1}
+                            { src: '../Content/image/student_add_image.jpg', description: 'Add a student' , id:1},
+                            { src: '../Content/image/data_view.jpg', description: 'Retrieve student data' , id:2}
         ];
+        $scope.template = [
+                            '../Scripts/Template/DefaultHomePage.html',
+                            '../Scripts/Template/AddStudent.html', 
+                            '../Scripts/Template/ViewStudentData.html',
+        ];
+        $scope.NameText = "Name:";
+        $scope.IDText = "ID:";
+        $scope.AgeText = "Age:";
+        $scope.viewID = 0;
+        $scope.display = function (id) {
+            $scope.viewID = id;
+        };
     };
 
     app.controller("MainController", MainController);
